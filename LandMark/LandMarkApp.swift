999e6edc623aa485726026214088417e801bc67f
KhuePM <minhkhuepham99@gmail.com>
2022-06-17 23:52:09 +0700

    Initial Commit

---
+++ LandMark/LandMarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarkApp.swift
+//  LandMark
+//
+//  Created by Phạm Minh Khuê on 17/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandMarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
